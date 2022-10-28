c8cdb891586760c8bb6c8f4fda25b3f327c42fcd
Alexander Dondokov <adondokov@gmail.com>
2022-10-28 16:37:36 +0800

    Edited URL tests

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
-from posts.models import Post, Group, User
+from ..models import Post, Group, User
 
 User = get_user_model()
 
 
-class TaskURLTests(TestCase):
+class PostURLTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -39,56 +39,76 @@ class TaskURLTests(TestCase):
         response = self.guest_client.get('/posts/<post_id>/')
         self.assertEqual(response.status_code, 200)
 
-    def test_post_id_edit_url_exists_at_desired_location(self):
-        response = self.authorized_client.get('/posts/<post_id>/')
-        self.assertEqual(response.status_code, 200)
+    def test_post_id_edit_url_exists_at_desired_location_for_client(self):
+        response = self.authorized_client.get(f'/posts/{PostURLTest.post.id}/edit/'/, follow=True)
+        self.assertRedirect(response, (f'/posts/{PostURLTest.post.id}/'))
 
-    def test_post_create_url_exists_at_desired_location(self):
+    def test_post_id_edit_url_exists_at_desired_location_for_author(self):
+        response = self.author_post.get(f'/posts/{PostURLTest.post.id}/edit/'/, follow=True)
+        self.assertRedirect(response, (f'/posts/{PostURLTest.post.id}//edit/'))
+
+    def test_post_create_url_exists_at_desired_location_for_authorized(self):
         response = self.authorized_client.get('/create/')
         self.assertEqual(response.status_code, 200)
+
+    def test_post_create_url_exists_at_desired_location_for_unauthorized(self):
+        response = self.authorized_client.get(f'/create/')
+        self.assertRedirect(response, ('/'))
 
     def test_unexisting_url_exists_at_desired_location(self):
         response = self.guest_client.get('/unexising_page/')
         self.assertEqual(response.status_code, 200)
 
-    # Проверяем доступность страниц для авторизованного пользователя
-    def test_task_list_url_exists_at_desired_location(self):
-        """Страница /task/ доступна авторизованному пользователю."""
-        response = self.authorized_client.get('/task/')
-        self.assertEqual(response.status_code, 200)
+#проверка шаблонов
 
-    def test_task_detail_url_exists_at_desired_location_authorized(self):
-        """Страница /task/test-slug/ доступна авторизованному
-        пользователю."""
-        response = self.authorized_client.get('/task/test-slug/')
-        self.assertEqual(response.status_code, 200)
-
-    # Проверяем редиректы для неавторизованного пользователя
-    def test_task_list_url_redirect_anonymous(self):
-        """Страница /task/ перенаправляет анонимного пользователя."""
-        response = self.guest_client.get('/task/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_task_detail_url_redirect_anonymous(self):
-        """Страница /task/test-slug/ перенаправляет анонимного
-        пользователя.
-        """
-        response = self.guest_client.get('/task/test-slug/')
-        self.assertEqual(response.status_code, 302)
-
-
-# posts/tests/test_urls.py
-from django.test import TestCase, Client
-
-
-class StaticURLTests(TestCase):
-    def setUp(self):
-        self.guest_client = Client()
-
-    def test_about_url_exists_at_desired_location(self):
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_about_url_uses_correct_template(self):
+    def test_home_url_exists_at_desired_location(self):
         response = self.guest_client.get('/')
         self.assertTemplateUsed(response, 'posts/index.html')
+
+    def test_group_slug_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/group/<slug>/')
+        self.assertTemplateUsed(response, 'posts/group_list.html')
+
+    def test_profile_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/profile/<username>/')
+        self.assertTemplateUsed(response, 'profile.html')
+
+    def test_post_id_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/posts/<post_id>/')
+        self.assertTemplateUsed(response, 'posts/detail.html')
+
+    def test_post_id_edit_url_exists_at_desired_location_for_client(self):
+        response = self.authorized_client.get(f'/posts/{PostURLTest.post.id}/edit/'/, follow=True)
+        self.assertTemplateUsed(response, 'create/post.html')
+
+    def test_post_id_edit_url_exists_at_desired_location_for_author(self):
+        response = self.author_post.get(f'/posts/{PostURLTest.post.id}/edit/'/, follow=True)
+        self.assertTemplateUsed(response, 'create/post.html')
+
+    def test_post_create_url_exists_at_desired_location_for_authorized(self):
+        response = self.authorized_client.get('/create/')
+        self.assertTemplateUsed(response, 'create/post.html')
+
+    def test_post_create_url_exists_at_desired_location_for_unauthorized(self):
+        response = self.authorized_client.get(f'/create/')
+        self.assertTemplateUsed(response, 'posts/index.html')
+
+    def test_unexisting_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/unexising_page/')
+        self.assertTemplateUsed(response, 'posts/index.html')
+
+# posts/tests/test_urls.py
+# from django.test import TestCase, Client
+#
+#
+# class StaticURLTests(TestCase):
+#     def setUp(self):
+#         self.guest_client = Client()
+#
+#     def test_about_url_exists_at_desired_location(self):
+#         response = self.guest_client.get('/')
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_about_url_uses_correct_template(self):
+#         response = self.guest_client.get('/')
+#         self.assertTemplateUsed(response, 'posts/index.html')
